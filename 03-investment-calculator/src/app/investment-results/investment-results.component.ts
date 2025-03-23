9b428c26e72906d093de04e997918a49611c6ee1
Snehal Raut <snehalraut@Snehals-Laptop.local>
2025-03-22 22:40:42 -0400

    Investment calculator updated and migrated to module.

--- 03-investment-calculator/src/app/investment-results/investment-results.component.ts
+++ 03-investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,26 +1,22 @@
-import { Component, input, Input } from '@angular/core';
-import { InvestMentInput } from '../investment-input.model';
+import { Component, computed, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-results = input<{
 
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  
-  }[]>();
+  private investmentService = inject(InvestmentService);
 
+  // constructor(private investmentService: InvestmentService){}
   
-  
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+
+  results = computed(() => this.investmentService.resultData());
 }
--- 03-investment-calculator/src/app/user-input/user-input.component.ts
+++ 03-investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,11 +1,11 @@
 import { Component, output, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestMentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
+  standalone: false,
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
@@ -18,13 +18,15 @@ export class UserInputComponent {
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+
+  constructor(private investmentService : InvestmentService) {
+
+  }
+
   onSubmit() {
-    console.log('submitted!')
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
-    this.calculate.emit({
+
+    this.investmentService.calculateInvestmentResults({
+
       initialInvestment: +this.enteredInitialInvestment(),
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
