cdd82f705a53b7091d3af1e039b835996e368173
Snehal Raut <snehalraut@Snehals-Laptop.local>
2025-03-26 17:52:05 -0400

    Standalone services in depth

---
+++ 09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, Inject, inject, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
+import { TasksServiceToken } from '../../../main';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+ 
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  
+
+  constructor(@Inject(TasksServiceToken) private tasksService:TasksService ){
+
+  }
+  onAddTask(title: string, description: string) {
+    this.tasksService.addTask({title, description});
+    this.formEl()?.nativeElement.reset();
+  }
+}
